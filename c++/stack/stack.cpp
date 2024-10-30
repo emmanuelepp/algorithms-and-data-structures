69e4bdff0ac6ac21357f1f84fa38b0a22b40fa76
emmanuelepp <emmanuelponciano@gmail.com>
2024-10-30 19:45:17 -0400

    stack implementation

---
+++ c++/stack/stack.cpp
@@ -0,0 +1,62 @@
+#include <iostream>
+class Node
+{
+public:
+    int value;
+    Node *next;
+    Node(int value) : value(value), next(nullptr) {}
+};
+
+class Stack
+{
+private:
+    Node *top;
+    int height;
+
+public:
+    Stack() : top(nullptr), height(0) {}
+
+    void printStack()
+    {
+        Node *current = top;
+        while (current)
+        {
+            std::cout << current << std::endl;
+            current = current->next;
+        }
+    }
+
+    void push(int value)
+    {
+        Node *newNode = new Node(value);
+        newNode->next = top;
+        top = newNode;
+        height++;
+    }
+
+    void pop()
+    {
+        if (top == nullptr)
+            return;
+        Node *newNode = top;
+        top = top->next;
+        delete newNode;
+        height--;
+    }
+
+    bool isEmpty()
+    {
+        return height == 0;
+    }
+    int getTop()
+    {
+        if (top == nullptr)
+            return;
+        return top->value;
+    }
+
+    int getSize()
+    {
+        return height;
+    }
+};
